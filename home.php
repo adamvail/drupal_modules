eba7692043a7a9ca0ffe64825cd14ff3c8a5745d
ACAHNN <acahn@wisc.edu>
2013-03-14 21:45:40 -0500

    strength table is now dynamic

--- home.php
+++ home.php
@@ -14,19 +14,6 @@
 	width:100%;
 	clear:both;
 }
-#one_half{
-	float:left;
-	width:40%;
-	margin:20px;
-	border-style:solid;
-	border-width:5px;
-}
-#two_half{
-	float:left;
-	padding: 0px 20px 0px 20px;
-	border-style:solid;
-	border-width:5px;
-}
 #title{
 	text-align:center;
 }
@@ -46,26 +33,19 @@ th,td{
 			print '<img src="' .  base_path() . drupal_get_path('theme', 'skeletontheme') . '/mockup/workout-motivation.jpg">';
 			db_query('SELECT @last_id := MAX(wid) FROM {workout_builder_conditioning}');
 			$result = db_query('SELECT * FROM {workout_builder_conditioning} WHERE wid = @last_id');
-			$query = db_query('SELECT wid FROM {workout_builder_conditioning} WHERE wid = @last_id');
-			$wid = 0;
-			foreach($query as $row){
-				$wid = $row->wid;
-			}
-				print '<table>';
-				
+			$wid = db_query('SELECT MAX(wid) FROM {workout_builder_conditioning}')->fetchField();
+				print '<table>';			
 					print '<tr>';
-						print '<th colspan="4"><a href="?q=workout_tracker&wid=' . $wid . '">' . 'Conditioning Portion:' . '</a></th>';	
+						print '<th colspan="0"><a href="?q=workout_tracker&wid=' . (int)$wid . '">' . 'Conditioning Portion:' . '</a></th>';	
 					print '</tr>';
-					
-					print '<tr>' ;
-						print '<th>' . 'Style' . '</th>';
-						print '<th>' . 'Duration' . '</th>';
-						print '<th>' . 'Reps' . '</th>';
-						print '<th>' . 'Movement' . '</th>';
-					print '</tr>';
+							print '<th>' . 'Style' . '</th>';
+							print '<th>' . 'Duration' . '</th>';
+							print '<th>' . 'Reps' . '</th>';
+							print '<th>' . 'Movement' . '</th>';
+						print '</tr>';
 					foreach ($result as $row) {
 						print '<tr>' ;
-							print '<td>' . $row->wid . '</td>';
+							print '<td>' . $row->style . '</td>';
 							print '<td>' . $row->duration . '</td>';
 							print '<td>' . $row->reps . '</td>';
 							print '<td>' . $row->movement . '</td>';
@@ -84,22 +64,96 @@ th,td{
 				print '<table>';
 				
 					print '<tr>';
-						print '<th colspan="4"><a href="?q=workout_tracker&wid=' . $wid . '">' . 'Strength Portion:' . '</a></th>';	
+						print '<th colspan="0"><a href="?q=workout_tracker&wid=' . (int)$wid . '">' . 'Strength Portion:' . '</a></th>';	
 					print '</tr>';
 					
-					print '<tr>' ;
-						print '<th>' . 'Sets' . '</th>';
-						print '<th>' . 'Reps' . '</th>';
-						print '<th>' . 'Movement' . '</th>';
-						print '<th>' . 'Weight (%)' . '</th>';
-					print '</tr>';
+					$i = 0;
 					foreach ($result as $row) {
-						print '<tr>' ;
-							print '<td>' . $row->sets . '</td>';
-							print '<td>' . $row->reps . '</td>';
-							print '<td>' . $row->movement . '</td>';
-							print '<td>' . $row->wgt_percentage . '</td>';
-						print '</tr>';
+						// write the table header per strength_id
+						if($i != $row->strength_id){
+							// weight as percentage header
+							if($row->wgt_style == 'percentage'){
+								print '<tr>' ;
+									print '<th>' . 'Clock (min)' . '</th>';
+									print '<th>' . 'Sets' . '</th>';
+									print '<th>' . 'Reps' . '</th>';
+									print '<th>' . 'Movement' . '</th>';
+									print '<th>' . 'Weight (%)' . '</th>';
+								print '</tr>';
+							}
+							// weight as specific header
+							elseif($row->wgt_style == 'weight'){ 
+								// weight as men and women header
+								if($row->wgt_men != NULL && $row->wgt_women != NULL){
+									print '<tr>' ;
+										print '<th>' . 'Clock (min)' . '</th>';
+										print '<th>' . 'Sets' . '</th>';
+										print '<th>' . 'Reps' . '</th>';
+										print '<th>' . 'Movement' . '</th>';
+										print '<th>' . 'Weight Men/Women (lbs)' . '</th>';
+									print '</tr>';
+								}
+								// weight as self created header
+								elseif($row->wgt_self_rx != NULL){
+									print '<tr>' ;
+										print '<th>' . 'Clock (min)' . '</th>';
+										print '<th>' . 'Sets' . '</th>';
+										print '<th>' . 'Reps' . '</th>';
+										print '<th>' . 'Movement' . '</th>';
+										print '<th>' . 'Weight (lbs)' . '</th>';
+									print '</tr>';															
+								}
+							}
+							elseif($row->wgt_style == 'ahap'){
+								print '<tr>' ;
+									print '<th>' . 'Clock (min)' . '</th>';
+									print '<th>' . 'Sets' . '</th>';
+									print '<th>' . 'Reps' . '</th>';
+									print '<th>' . 'Movement' . '</th>';
+									print '<th>' . 'Weight (AHAP)' . '</th>';
+								print '</tr>';							
+							}
+							$i = $row->strength_id;
+						}
+						// write the values of the workout
+						if($row->wgt_style == 'percentage'){						
+							print '<tr>' ;
+								print '<td>' . $row->clock . '</td>';
+								print '<td>' . $row->sets . '</td>';
+								print '<td>' . $row->reps . '</td>';
+								print '<td>' . $row->movement . '</td>';
+								print '<td>' . $row->wgt_percentage . '</td>';
+							print '</tr>';
+						}
+						elseif($row->wgt_style == 'weight'){
+							if($row->wgt_men != NULL && $row->wgt_women != NULL){						
+								print '<tr>' ;
+									print '<td>' . $row->clock . '</td>';
+									print '<td>' . $row->sets . '</td>';
+									print '<td>' . $row->reps . '</td>';
+									print '<td>' . $row->movement . '</td>';
+									print '<td>' . $row->wgt_men . '/' . $row->wgt_women . '</td>';
+								print '</tr>';
+							}
+							elseif($row->wgt_self_rx != NULL){
+								print '<tr>' ;
+									print '<td>' . $row->clock . '</td>';
+									print '<td>' . $row->sets . '</td>';
+									print '<td>' . $row->reps . '</td>';
+									print '<td>' . $row->movement . '</td>';
+									print '<td>' . $row->wgt_self_rx . '</td>';
+								print '</tr>';				
+							}		
+						}
+						elseif($row->wgt_style == 'ahap'){
+							print '<tr>' ;
+								print '<td>' . $row->clock . '</td>';
+								print '<td>' . $row->sets . '</td>';
+								print '<td>' . $row->reps . '</td>';
+								print '<td>' . $row->movement . '</td>';
+								print '<td>' . 'As Heavy As Possible' . '</td>';
+							print '</tr>';	
+						}			
 					}
 				print '</table>';
 
