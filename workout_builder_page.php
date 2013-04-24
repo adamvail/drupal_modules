f6866451f9e2dc19dd304d2dcbb5265bcb2b1eb5
ACAHNN <acahn@wisc.edu>
2013-04-24 01:48:52 -0500

    converting the modules to actual pages on the website. might be useful in the future for more extensive theming, integration with other components, and allows for google analytics tracking

---
+++ workout_builder_page.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-40201944-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+</head>
+
+<body>						
+	<?php
+	
+		//check include statements
+		if(!@include '/var/www/sites/all/modules/workout_builder/workout_builder_wbuild.inc') {
+			include 'C:/wamp/www/drupal/sites/all/modules/workout_builder/workout_builder_wbuild.inc';
+			render_page();
+		}
+		else {
+			render_page();
+		}
+		function render_page() {
+			//render the workout results form to the page
+			print '<div id="form">';
+				print drupal_render(drupal_get_form('workout_builder_wbuilder')); 
+			print '</div>'; //end form div
+		}
+	?>
+</body>
+
+</html>
